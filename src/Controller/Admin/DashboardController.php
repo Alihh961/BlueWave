c24ae57567bee6c1c4b115612716812d7c9f2094
Alihh961 <hajhassan.ali@outlook.com>
2023-12-19 22:09:37 +0100

    before adding unicons

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Order;
+use App\Entity\OrderStatusHistory;
+use App\Entity\Status;
+use App\Entity\Transaction;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator
+    )
+    {
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $url = $this->adminUrlGenerator
+            ->setController(UserCrudController::class)
+            ->generateUrl();
+
+
+        return $this->redirect($url);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('BlueWave');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToUrl('Go to Home Page', 'fa fa-home', $this->generateUrl('app_home'))
+        ->setCssClass("bg-primary p-2");
+
+        yield MenuItem::linkToDashboard('Dashboard Page', 'fa-solid fa-cruzeiro-sign');
+
+        yield MenuItem::subMenu("Users", 'fa fa-user')
+            ->setSubItems([
+                MenuItem::linkToCrud("Create a user", "fa fa-plus", User::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud("All users", "fa fa-eye", User::class)->setAction(Crud::PAGE_INDEX)
+
+            ]);
+
+        yield MenuItem::subMenu("Status", 'fa fa-code-compare')
+            ->setSubItems([
+                MenuItem::linkToCrud("Create a status", "fa fa-plus", Status::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud("All statuses", "fa fa-eye", Status::class)->setAction(Crud::PAGE_INDEX)
+
+            ]);
+
+        yield MenuItem::subMenu("Order", 'fa-solid fa-cubes')
+            ->setSubItems([
+                MenuItem::linkToCrud("All orders", "fa fa-eye", Order::class)->setAction(Crud::PAGE_INDEX)
+
+            ]);
+
+        yield MenuItem::subMenu("Orders History", 'fa-solid fa-clock-rotate-left')
+            ->setSubItems([
+                MenuItem::linkToCrud("All orders history", "fa fa-eye", OrderStatusHistory::class)->setAction(Crud::PAGE_INDEX)
+
+            ]);
+
+        yield MenuItem::subMenu("Transactions", 'fa-regular fa-newspaper')
+            ->setSubItems([
+                MenuItem::linkToCrud("All transactions", "fa fa-eye", Transaction::class)->setAction(Crud::PAGE_INDEX)
+
+            ]);
+    }
+}
